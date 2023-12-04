3bcf8a6aa5fddb1998e2aed892156053e0036aeb
jay <dj.min43@gmail.com>
2023-12-04 18:41:36 +0900

    day 1 part 1

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,5 +5,5 @@ S
 build.gradle.kts,d/b/dbcff70658daf80b53ce624f6adcaa529df5ed8d
 X
 (src/main/java/org/example/day1/Day1.java,e/5/e52c8ad2cb67884e02c7937e66c72425044fec8c
-X
-(src/main/java/org/example/day1/input.txt,6/f/6fbab676c804010b09178ba0fadf8bf0ee98ba1f
+]
+-src/main/java/org/example/day1/day1-input.txt,2/f/2f674c4d120e2c063b496b50d1cb55bee451439e
--- src/main/java/org/example/day1/Day1.java
+++ src/main/java/org/example/day1/Day1.java
@@ -1,4 +1,40 @@
 package org.example.day1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Day1 {
+
+    public static void main(String[] args) throws FileNotFoundException  {
+
+
+        File day1Input = new File("./day1-input.txt");
+        Scanner reader = new Scanner(day1Input);
+        ArrayList<String> inputList = new ArrayList<>();
+        while (reader.hasNextLine()) {
+            inputList.add(reader.nextLine());
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < inputList.size(); i++) {
+            String code = inputList.get(i);
+            ArrayList<Character> charList = new ArrayList<>();
+            for (int j = 0; j < code.length(); j++) {
+                if (Character.isDigit(code.charAt(j)))
+                charList.add(code.charAt(j));
+            }
+            String total = String.valueOf(charList.get(0)) + String.valueOf(charList.get(charList.size() - 1));
+            sum += Integer.parseInt(total);
+        }
+
+        int partOneAnswer = sum;
+        System.out.println("partOneAnswer = " + partOneAnswer);
+
+
+    }
+
+
 }
