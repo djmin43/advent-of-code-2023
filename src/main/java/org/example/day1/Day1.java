36aabe86ee28732c8dc9a158ddebcc9c3aba8a91
jay <dj.min43@gmail.com>
2023-12-04 18:56:01 +0900

    refactor

--- src/main/java/org/example/day1/Day1.java
+++ src/main/java/org/example/day1/Day1.java
@@ -2,21 +2,29 @@ package org.example.day1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Day1 {
 
-    public static void main(String[] args) throws FileNotFoundException  {
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String> inputList = readInput();
+        partOne(inputList);
+    }
 
+    private static void partTwo(ArrayList<String> inputList) {
+        for (int i = 0; i < inputList.size(); i++) {
+            String code = inputList.get(i);
+            ArrayList<Character> charList = new ArrayList<>();
 
-        File day1Input = new File("./day1-input.txt");
-        Scanner reader = new Scanner(day1Input);
-        ArrayList<String> inputList = new ArrayList<>();
-        while (reader.hasNextLine()) {
-            inputList.add(reader.nextLine());
         }
 
+
+    }
+
+    private static void partOne(ArrayList<String> inputList) {
         int sum = 0;
 
         for (int i = 0; i < inputList.size(); i++) {
@@ -24,7 +32,7 @@ public class Day1 {
             ArrayList<Character> charList = new ArrayList<>();
             for (int j = 0; j < code.length(); j++) {
                 if (Character.isDigit(code.charAt(j)))
-                charList.add(code.charAt(j));
+                    charList.add(code.charAt(j));
             }
             String total = String.valueOf(charList.get(0)) + String.valueOf(charList.get(charList.size() - 1));
             sum += Integer.parseInt(total);
@@ -33,8 +41,18 @@ public class Day1 {
         int partOneAnswer = sum;
         System.out.println("partOneAnswer = " + partOneAnswer);
 
-
     }
+
+    private static ArrayList<String> readInput() throws FileNotFoundException {
+        File day1Input = new File("./day1-input.txt");
+        Scanner reader = new Scanner(day1Input);
+        ArrayList<String> inputList = new ArrayList<>();
+        while (reader.hasNextLine()) {
+            inputList.add(reader.nextLine());
+        }
+        return inputList;
+    }
+
 
 
 }
