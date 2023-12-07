237393e9d3b2fee0179776dadb7cebf892f5f8a1
jay <dj.min43@gmail.com>
2023-12-08 08:25:11 +0900

    parsing cubecondrum string list

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,12 +3,6 @@ S
 #src/main/java/org/example/Main.java,0/b/0ba1b9c7a4878802f938e331cc2d8a042087ec6b
 @
 build.gradle.kts,d/b/dbcff70658daf80b53ce624f6adcaa529df5ed8d
-X
-(src/main/java/org/example/day1/Day1.java,e/5/e52c8ad2cb67884e02c7937e66c72425044fec8c
-]
--src/main/java/org/example/day1/day1-input.txt,2/f/2f674c4d120e2c063b496b50d1cb55bee451439e
-\
-,src/test/java/org/example/day1/Day1Test.java,a/a/aa97bef8883fd2d56fe84719f00c0724f3a6baf3
 a
 1src/main/java/org/example/day2/CubeConundrum.java,0/0/00a79252bc91451365ca25597158880af928b760
 Q
--- src/main/java/org/example/day2/CubeConundrum.java
+++ src/main/java/org/example/day2/CubeConundrum.java
@@ -1,7 +1,20 @@
 package org.example.day2;
 
+import org.example.utils.InputUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CubeConundrum {
     public static void main(String[] args) {
+        ArrayList<String> inputList = InputUtil.readInput(2);
+        ArrayList<ArrayList<String>> splitList = new ArrayList<>();
 
+        for (String inputString : inputList) {
+            String[] substrings = inputString.split("[;:,]");
+            splitList.add(new ArrayList<>(Arrays.asList(substrings)));
+        }
+
+        System.out.println("splitList = " + splitList.get(0));
     }
 }
